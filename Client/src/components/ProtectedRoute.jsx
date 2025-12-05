1aadf29d76c2624a79c0f9a588b175b33c5a990d
divyaprabhag <divyagprabha@gmail.com>
2025-12-05 14:17:47 +0530

    Initial push

--- Client/src/components/ProtectedRoute.jsx
+++ Client/src/components/ProtectedRoute.jsx
@@ -1,18 +1,3 @@
-// // src/components/ProtectedRoute.jsx
-// import React, { useContext } from 'react';
-// import { Navigate } from 'react-router-dom';
-// import { AuthContext } from '../context/AuthContext';
-
-// const ProtectedRoute = ({ children }) => {
-//   const { isAuthenticated } = useContext(AuthContext);
-//   return isAuthenticated ? children : <Navigate to="/login" replace />;
-// };
-
-// export default ProtectedRoute;
-
-
-
-
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
--- Server/models/order.js
+++ Server/models/order.js
@@ -1,21 +1,3 @@
-// //models/order.js
-// const mongoose = require('mongoose');
-
-// const orderSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   address: String,
-//   contact: String,
-//   products: Array,
-//   totalAmount: Number,
-//   orderDate: { type: Date, default: Date.now }
-// });
-
-// module.exports = mongoose.model('Order', orderSchema);
-
-
-
-
 //models/order.js
 const mongoose = require('mongoose');
 
